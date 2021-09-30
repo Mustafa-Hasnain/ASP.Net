d9a80acca4a66d6aff25f440b58a83aca05e53de
Mustafa-Hasnain <mustafahasnain19@gmail.com>
2021-09-30 19:30:29 +0500

    Task

---
+++ Code First Approach/Code First Approach/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Code_First_Approach.Models
+{
+    public class EmployeeContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Code First Approach/Code First Approach/Models/EmployeeView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Code_First_Approach.Models
+{
+    public class EmployeeView
+    {
+        public List<Employee> getData()
+        {
+            EmployeeContext ec = new EmployeeContext();
+            var emp = ec.Employees.ToList();
+            return emp;
+        }
+    }
+}
